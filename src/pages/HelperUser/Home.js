d465357f95403a658e2c38faf18cb73b29119193
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2020-12-09 01:07:14 +0500

    home page design is ready

--- src/pages/HelperUser/Home.js
+++ src/pages/HelperUser/Home.js
@@ -1,8 +1,37 @@
 import React from "react";
-
+import Paper from "@material-ui/core/Paper";
+import {useSelector} from "react-redux";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import Switch from "@material-ui/core/Switch";
 const Home = ()=>{
+	const [isAvailable, setIsAvailable] = React.useState(true);
+	const stateProps = useSelector(({User})=>{
+		return {...User};
+	});
+	const { data } = stateProps;
 	return (
-		<div> <h1>this is home</h1></div>
+		<div className={"container"} >
+			<h1>{data.fullName}</h1>
+			<div className={"d-flex flex-direction-row"}>
+				<Paper elevation={0} className={"m-4 p-4"} >
+					<p>	Are you available to help? </p>
+					<Switch
+						checked={isAvailable}
+						onChange={(e)=>setIsAvailable(e.target.checked)}
+						color="primary"
+						name="checkedB"
+						inputProps={{ "aria-label": "primary checkbox" }}
+					/>
+				</Paper>
+				<Paper elevation={0} className={"m-4 p-4 d-flex flex-column align-items-center help-container"} >
+					<p>	I need help! </p>
+					<NearMeIcon fontSize="large" />
+				</Paper>
+			</div>
+			<p>If you switch toggle to ‘yes,’ keep this tab open; </p><p>You can focus on other tabs. You’ll receive a notification if someone needs help.
+			</p>
+			<p>Your google meet to help others is: {data.meetLink}</p>
+		</div>
 	);
 };
 
