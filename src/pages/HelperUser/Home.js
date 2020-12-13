70f85a9db1d83eba8fd81db5db2fe949943b0e5c
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2020-12-13 21:12:23 +0500

    fixed

--- src/pages/HelperUser/Home.js
+++ src/pages/HelperUser/Home.js
@@ -20,7 +20,7 @@ const Home = ()=>{
 			database
 				.ref("helpers").child(auth.currentUser.uid).on("value", (snapshot) => {
 					dispatch(updateHelperUserStatus({status: snapshot && snapshot.val() && Object.entries(snapshot.val()).length>1?snapshot.val().status : helperStatus.AVAILABLE}));
-					dispatch(getHelperUserData(Object.entries(snapshot.val()).length>2?snapshot.val():{assignedUser:""}));
+					dispatch(getHelperUserData(snapshot && snapshot.val() && Object.entries(snapshot.val()).length>2?snapshot.val():{assignedUser:""}));
 				});
 		}
 		catch (e) {
